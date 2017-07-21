56b1b772ca59148567b52df5d1092e39912acfa7
Lucian Carata <lc525@cam.ac.uk>
2017-07-21 13:28:33 +0100

    Improve the C-facing API

--- libopus.rust/src/lib.rs
+++ libopus.rust/src/lib.rs
@@ -1,13 +1,17 @@
 extern crate neo4j;
 extern crate packstream;
+extern crate libc;
 
 use std::mem::transmute;
+use std::ffi::CStr;
+use std::str::Utf8Error;
+
 
 #[repr(C)]
 #[derive(Debug)]
 pub enum CfgMode {
     Auto,
-    Advanced
+    Advanced,
 }
 
 #[repr(C)]
@@ -21,10 +25,11 @@ pub struct AdvancedConfig {
 #[derive(Debug)]
 pub struct Config {
     cfg_mode: CfgMode,
-    db_user: String,
-    db_password: String,
-    cfg_detail: AdvancedConfig,
+    db_user: *mut libc::c_char,
+    db_password: *mut libc::c_char,
+    cfg_detail: Option<AdvancedConfig>,
 }
+
 
 pub struct LibOpus {
     cfg: Config,
@@ -33,27 +38,34 @@ pub struct LibOpus {
 #[repr(C)]
 pub struct OpusHdl(LibOpus);
 
+fn str_from_c_char<'a>(val: *const libc::c_char) -> Result<&'a str, Utf8Error> {
+    unsafe { CStr::from_ptr(val).to_str() }
+}
+
 #[no_mangle]
-pub extern fn opus_init(cfg: Config) -> *mut OpusHdl {
-    unsafe {
-        transmute(Box::new( OpusHdl(LibOpus{ cfg: cfg }) ))
+pub extern "C" fn opus_init(cfg: Config) -> *mut OpusHdl {
+    unsafe { transmute(Box::new(OpusHdl(LibOpus { cfg: cfg }))) }
+}
+
+#[no_mangle]
+pub extern "C" fn print_cfg(hdl: *const OpusHdl) {
+    let ref hdl = unsafe { &*hdl }.0;
+    println!("LibOpus Config {:?}", hdl.cfg);
+}
+
+#[no_mangle]
+pub extern "C" fn process_event(hdl: *mut OpusHdl, event: *const libc::c_char) {
+    let ref mut hdl = unsafe { &mut *hdl }.0;
+    let user = str_from_c_char(hdl.cfg.db_user);
+    let event = str_from_c_char(event);
+    match event {
+        Ok(slice) => println!("User {} Processing event: {}", user.unwrap(), slice),
+        Err(e) => println!("Error parsing string {}", e),
     }
 }
 
 #[no_mangle]
-pub extern fn print_cfg(hdl: *const OpusHdl) {
-    let ref hdl = unsafe { & *hdl }.0;
-    println!("I'm now processing an event! Config {:?}", hdl.cfg);
-}
-
-#[no_mangle]
-pub extern fn process_event(hdl: *mut OpusHdl, event: String) {
-    let ref hdl = unsafe { & *hdl }.0;
-    println!("Processing event {} in db with user {}", event, hdl.cfg.db_user);
-}
-
-#[no_mangle]
-pub extern fn opus_cleanup(hdl: *mut OpusHdl) {
+pub extern "C" fn opus_cleanup(hdl: *mut OpusHdl) {
     let _drop_me: Box<OpusHdl> = unsafe { transmute(hdl) };
 }
 
