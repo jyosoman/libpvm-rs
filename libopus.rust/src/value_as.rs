849694361d19a92aec0e0113086f50d8490bca0f
HarkonenBade <github@harkonen.net>
2017-07-22 19:13:01 +0100

    Added value as i64 and as &Vec

--- libopus.rust/src/value_as.rs
+++ libopus.rust/src/value_as.rs
@@ -2,15 +2,24 @@ use packstream::values::Value;
 
 pub trait CastValue {
     fn as_string(&self) -> Option<String>;
+    fn as_i64(&self) -> Option<i64>;
     fn as_u64(&self) -> Option<u64>;
     fn as_i32(&self) -> Option<i32>;
     fn as_bool(&self) -> Option<bool>;
+    fn as_vec_ref(&self) -> Option<&Vec<Value>>;
 }
 
 impl CastValue for Value {
     fn as_string(&self) -> Option<String> {
         match self {
             &Value::String(ref s) => Some(s.clone()),
+            _ => None,
+        }
+    }
+
+    fn as_i64(&self) -> Option<i64> {
+        match self {
+            &Value::Integer(i) => Some(i),
             _ => None,
         }
     }
@@ -32,6 +41,13 @@ impl CastValue for Value {
     fn as_bool(&self) -> Option<bool> {
         match self {
             &Value::Boolean(b) => Some(b),
+            _ => None,
+        }
+    }
+
+    fn as_vec_ref(&self) -> Option<&Vec<Value>> {
+        match self {
+            &Value::List(ref l) => Some(l),
             _ => None,
         }
     }
