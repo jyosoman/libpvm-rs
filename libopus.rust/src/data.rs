1641ea64295a80a2c5f44b2dda7d5e10d71b14a0
HarkonenBade <github@harkonen.net>
2017-07-22 17:07:36 +0100

    Simplify value parsing code

--- libopus.rust/src/data.rs
+++ libopus.rust/src/data.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 
 use packstream::values::{Value, ValueCast};
 
+use value_as::CastValue;
+
 #[derive(Debug)]
 pub struct NodeID(pub u64);
 
@@ -16,6 +18,10 @@ impl Node {
             Value::Structure { signature, fields } => {
                 assert!(signature == 0x4E);
                 assert!(fields.len() == 3);
+                let id = match fields[0] {
+                    Value::Integer(ref i) => i,
+                    _ => panic!(),
+                };
                 let labs = match fields[1] {
                     Value::List(ref l) => l,
                     _ => panic!(),
@@ -69,51 +75,31 @@ impl ProcessNode {
     }
 
     pub fn from_props(props: &HashMap<String, Value>) -> Result<ProcessNode, &'static str> {
-        let db_id = match props.get("db_id") {
-            Some(v) => {
-                match v {
-                    &Value::Integer(i) => ::data::NodeID(i as u64),
-                    _ => return Err("db_id property is not an Integer"),
-                }
-            }
-            None => return Err("Missing db_id property"),
-        };
-        let cmdline = match props.get("cmdline") {
-            Some(v) => {
-                match v {
-                    &Value::String(ref s) => s.clone(),
-                    _ => return Err("cmdline property is not a String"),
-                }
-            }
-            None => return Err("Missing cmdline property"),
-        };
-        let uuid = match props.get("uuid") {
-            Some(v) => {
-                match v {
-                    &Value::String(ref s) => s.clone(),
-                    _ => return Err("uuid property is not a String"),
-                }
-            }
-            None => return Err("Missing uuid property"),
-        };
-        let pid = match props.get("pid") {
-            Some(v) => {
-                match v {
-                    &Value::Integer(i) => i as i32,
-                    _ => return Err("pid property is not an Integer"),
-                }
-            }
-            None => return Err("Missing pid property"),
-        };
-        let thin = match props.get("thin") {
-            Some(v) => {
-                match v {
-                    &Value::Boolean(b) => b,
-                    _ => return Err("thin property is not a bool"),
-                }
-            }
-            None => return Err("Missing thin property"),
-        };
+        let db_id = ::data::NodeID(props
+            .get("db_id")
+            .ok_or("Missing db_id property")?
+            .as_u64()
+            .ok_or("db_id property is not an Integer")?);
+        let cmdline = props
+            .get("cmdline")
+            .ok_or("Missing cmdline property")?
+            .as_string()
+            .ok_or("cmdline property is not a String")?;
+        let uuid = props
+            .get("uuid")
+            .ok_or("Missing uuid property")?
+            .as_string()
+            .ok_or("uuid property is not a String")?;
+        let pid = props
+            .get("pid")
+            .ok_or("Missing pid property")?
+            .as_i32()
+            .ok_or("pid property is not an Integer")?;
+        let thin = props
+            .get("thin")
+            .ok_or("Missing thin property")?
+            .as_bool()
+            .ok_or("thin property is not a bool")?;
         Ok(ProcessNode {
             db_id: db_id,
             cmdline: cmdline,
