bbda0d35a17c02abd1bc2ebf75b2f94c63104429
Thomas Bytheway <tb403@cam.ac.uk>
2017-08-07 18:03:58 +0100

    Strip back node/edge association

--- libopus.rust/src/persist.rs
+++ libopus.rust/src/persist.rs
@@ -73,12 +73,7 @@ pub fn persist_node(cypher: &mut CypherStream, node: &Node) -> Result<(), &'stat
          SET p.uuid = {uuid}
          SET p.cmdline = {cmdline}
          SET p.pid = {pid}
-         SET p.thin = {thin}
-         WITH p
-         FOREACH (ch IN {chs} |
-             MERGE (q:Process {db_id: ch.id})
-             MERGE (p)-[e:INF]->(q)
-             SET e.class = ch.class)",
+         SET p.thin = {thin}",
         node.get_props(),
     );
     cypher.fetch_summary(&result);
--- libopus.rust/src/query/low.rs
+++ libopus.rust/src/query/low.rs
@@ -11,9 +11,7 @@ pub fn nodes_by_uuid(cypher: &mut CypherStream, uuid: &str) -> Vec<Node> {
     props.insert("uuid", uuid.from());
     let result = cypher.run(
         "MATCH (n {uuid: {uuid}})
-         WITH n
-         OPTIONAL MATCH (n)-[e]->(m)
-         RETURN n, collect(e)",
+         RETURN n",
         props,
     );
     let mut records: VecDeque<Data> = VecDeque::new();
@@ -23,7 +21,7 @@ pub fn nodes_by_uuid(cypher: &mut CypherStream, uuid: &str) -> Vec<Node> {
     let mut ret = Vec::with_capacity(records.len());
     for rec in records.drain(..) {
         match rec {
-            Data::Record(mut v) => ret.push(Node::from_value(v.remove(0), v.remove(0)).unwrap()),
+            Data::Record(mut v) => ret.push(Node::from_value(v.remove(0)).unwrap()),
         }
     }
     ret
