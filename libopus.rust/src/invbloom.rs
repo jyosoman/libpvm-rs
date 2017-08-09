1224831e135a6929c4799ecfc04050cdebcdabf2
Lucian Carata <lc525@cam.ac.uk>
2017-08-09 02:18:54 +0100

    Convert invbloom to use atomic operations

--- libopus.rust/src/invbloom.rs
+++ libopus.rust/src/invbloom.rs
@@ -3,24 +3,24 @@
  * https://www.somethingsimilar.com/2012/05/21/the-opposite-of-a-bloom-filter/
  */
 
-use std::sync::Mutex;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
-use std::cell::Cell;
 use trace::uuid5;
 
-const N: usize = 256;
+const N: usize = 256;  // have to pick power of 2
+const NMASK: usize = N - 1;
 
 #[derive(Default)]
 pub struct InvBloom {
-    data: Vec<Mutex<Cell<usize>>>,
+    data: Vec<AtomicUsize>,
 }
 
 impl InvBloom {
     pub fn new() -> InvBloom {
         let mut data = Vec::with_capacity(N);
         for _ in 0..N {
-            data.push(Mutex::new(Cell::new(0)));
+            data.push(AtomicUsize::new(0));
         }
         InvBloom { data: data }
     }
@@ -31,7 +31,7 @@ impl InvBloom {
             test.hash(&mut hasher);
             hasher.finish() as usize
         };
-        let prev = self.data[hash % N].lock().unwrap().replace(hash);
+        let prev = self.data[hash & NMASK].swap(hash, Ordering::Relaxed);
         prev == hash
     }
 }
