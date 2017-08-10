09e47f6c2b5a31adfd62fde78277bbe16001987f
Thomas Bytheway <tb403@cam.ac.uk>
2017-08-10 15:44:07 +0100

    Clear out old tests

--- libopus.rust/src/lib.rs
+++ libopus.rust/src/lib.rs
@@ -27,39 +27,6 @@ pub mod invbloom;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use super::query::low;
-    use neo4j::cypher::CypherStream;
-    use std::time::Instant;
-    use trace::Uuid5;
-
-    const NANO: f64 = 1e9_f64;
-
     #[test]
     fn it_works() {}
-
-    #[test]
-    fn test_cypher() {
-        let p = data::Node::Process(data::ProcessNode {
-            db_id: data::NodeID(0),
-            uuid: Uuid5::zero(),
-            cmdline: String::from("./foo"),
-            pid: 2,
-            thin: false,
-        });
-        let q = data::Node::Process(data::ProcessNode {
-            db_id: data::NodeID(1),
-            uuid: Uuid5::zero(),
-            cmdline: String::from("./bar"),
-            pid: 1,
-            thin: false,
-        });
-        let mut cypher = CypherStream::connect("localhost:7687", "neo4j", "opus").unwrap();
-        let i = Instant::now();
-        persist::persist_node(&mut cypher, &q);
-        persist::persist_node(&mut cypher, &p);
-        println!("{:?}", (i.elapsed().subsec_nanos() as f64) / NANO);
-        let foo = low::nodes_by_uuid(&mut cypher, "0000-0000-0000");
-        println!("{:?}", foo);
-    }
 }
