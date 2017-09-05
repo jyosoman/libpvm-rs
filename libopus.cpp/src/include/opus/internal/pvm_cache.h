122201efde9eb29107e308988bf95313388176f7
Thomas Bytheway <tb403@cam.ac.uk>
2017-09-05 15:30:06 +0100

    Initial files for pvm_cache

---
+++ libopus.cpp/src/include/opus/internal/pvm_cache.h
@@ -0,0 +1,13 @@
+// Copyright [2017] <Thomas Bytheway & Lucian Carata>
+//
+// Created by tb403 on 05/09/17.
+//
+
+#ifndef LIBOPUS_CPP_SRC_INCLUDE_OPUS_INTERNAL_PVMCACHE_H_
+#define LIBOPUS_CPP_SRC_INCLUDE_OPUS_INTERNAL_PVMCACHE_H_
+
+class PVMCache {
+
+};
+
+#endif  // LIBOPUS_CPP_SRC_INCLUDE_OPUS_INTERNAL_PVMCACHE_H_
---
+++ libopus.cpp/src/lib/pvm_cache.cpp
@@ -0,0 +1,6 @@
+// Copyright [2017] <Thomas Bytheway & Lucian Carata>
+//
+// Created by tb403 on 05/09/17.
+//
+
+#include "../include/opus/internal/pvm_cache.h"
