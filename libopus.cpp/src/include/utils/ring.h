9326f096bd468d3ff70ea5d26962e189f5cb5463
Lucian Carata <lc525@cam.ac.uk>
2017-09-09 02:29:41 +0100

    Adds ringbuffer producer slot reservations

--- libopus.cpp/src/include/utils/ring.h
+++ libopus.cpp/src/include/utils/ring.h
@@ -120,11 +120,29 @@ namespace utils {
        */
 
       /**
+       * As a producer, reserve a number of slots in the buffer. Make them
+       * all available to a consumer at the same time by calling release
+       */
+      void reserve(size_t nr_slots);
+
+      /**
+       * As a producer, release the reserved slots to consumers.
+       */
+      void release();
+
+      /**
        * Add an element to the ring buffer (from a producer thread). This
-       * method is threadsafe.
+       * method is threadsafe. Do not use if you have reserved slots in the
+       * buffer and you haven't called release yet. Use push_reserved instead.
        * \param ptr the element to add
        */
       void push(T* ptr);
+
+      /**
+       * Add an element into a reserved ring buffer slot. This needs to be
+       * called after reserve(..) and before release(..)
+       */
+      bool push_reserved(T* ptr, size_t slot);
 
       /**
        * Rmove element from the ring buffer (from a consumer thread). This
@@ -174,10 +192,14 @@ namespace utils {
       size_r *pos_thread_;
       size_r *pos_prod_, *pos_cons_;
 
+
       // Actual buffer storage. The memory is aligned on page boundary on
       // allocation, in the Ring constructor
       T **ring_buf_;
 
+      // res_prod_ is another array indexed by current ThrId, maintaining the
+      // number of slots reserved by the respective producer
+      size_r *res_prod_;
   };
 
   /**
@@ -203,6 +225,11 @@ namespace utils {
     pos_prod_ = pos_thread_;
     pos_cons_ = pos_thread_ + max_prod_thrid_ + 1;
 
+    ::posix_memalign((void**)&res_prod_,
+                     PAGE_SIZE,
+                     sizeof(size_r) * max_prod_thrid_);
+    std::memset((void*)res_prod_, 0x0, sizeof(size_r) * max_prod_thrid);
+
     ::posix_memalign((void**)&ring_buf_,
                      PAGE_SIZE,
                      sizeof(void*) * R_SIZE);
@@ -215,6 +242,43 @@ namespace utils {
   Ring<T, R_SIZE, ThrId>::~Ring() {
     ::free(ring_buf_);
     ::free(pos_thread_);
+  }
+
+  // void reserve(size_t nr_slots);
+  template< typename T, size_r R_SIZE, decltype(get_thr_id) ThrId>
+  void Ring<T, R_SIZE, ThrId>::reserve(size_t nr_slots) {
+    assert(ThrId() <= max_prod_thrid_);
+    pos_prod_[ThrId()] = head_;
+    pos_prod_[ThrId()] = __sync_fetch_and_add(&head_, nr_slots);
+    // wait while ring buffer is full
+    while(unlikely(pos_prod_[ThrId()] >= last_tail_ + R_SIZE)) {
+      std::this_thread::yield();
+      //::usleep(10);
+
+      size_r min = tail_;
+
+      // update last_tail_;
+      for(size_t i = 0; i < max_cons_thrid_; ++i) {
+        size_r tmp_t = pos_cons_[i];
+
+        asm volatile("" ::: "memory");
+
+        if(tmp_t < min)
+          min = tmp_t;
+      }
+
+      last_tail_ = min;
+    }
+
+    res_prod_[ThrId()] = nr_slots;
+  }
+
+  // void release();
+  template< typename T, size_r R_SIZE, decltype(get_thr_id) ThrId>
+  void Ring<T, R_SIZE, ThrId>::release() {
+    assert(ThrId() <= max_prod_thrid_);
+    pos_prod_[ThrId()] = SIZE_R_MAX;
+    res_prod_[ThrId()] = 0;
   }
 
   // void Ring::push(T* ptr)
@@ -259,6 +323,16 @@ namespace utils {
 
     // consumers can process the added item
     pos_prod_[ThrId()] = SIZE_R_MAX;
+  }
+
+  //void push_reserved(T* ptr, size_t slot);
+  template< typename T, size_r R_SIZE, decltype(get_thr_id) ThrId>
+  bool Ring<T, R_SIZE, ThrId>::push_reserved(T* ptr, size_t slot) {
+    if(slot < res_prod_[ThrId()]){
+      ring_buf_[ pos_prod_[ThrId() + slot] & R_MASK ] = ptr;
+      return true;
+    }
+    return false;
   }
 
   // T* Ring::pop()
