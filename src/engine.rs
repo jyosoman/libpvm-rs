86f8e4f79a78c7bcff65a4b390786da91fdb0b75
Lucian Carata <lc525@cam.ac.uk>
2017-11-13 01:28:05 +0000

    path changes moving to rust-only repo

---
+++ src/engine.rs
@@ -0,0 +1,22 @@
+use futures::executor::Executor;
+use futures::future::Future;
+use futures_cpupool::Builder;
+
+struct StreamHashSplitter {
+}
+
+impl Executor for ProcessingPool {
+    fn execute(&self, r: Run) {
+    }
+}
+
+fn cpu_pool_init() {
+    let mut cpuBpool = Builder::new();
+    let mut ioBpool = Builder::new();
+    cpuBpool.pool_size += 2;
+    cpuBpool.name_prefix = Some(String::from("opus-cpu"));
+    cpuBpool.create();
+    ioBpool.pool_size += 4;
+    ioBpool.name_prefix = Some(String::from("opus-io"));
+    ioBpool.create();
+}
