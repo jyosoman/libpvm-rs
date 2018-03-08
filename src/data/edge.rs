d243532c7509ffb4cc377e3290ca4ab79943a7b2
Thomas Bytheway <tb403@cam.ac.uk>
2018-03-08 14:15:01 +0000

    Move Edge structures out to their own file

---
+++ src/data/edge.rs
@@ -0,0 +1,52 @@
+use packstream::values::Value;
+use value_as::CastValue;
+
+use super::NodeID;
+
+pub enum Edge {
+    Child(NodeID),
+    Next(NodeID),
+}
+
+impl Edge {
+    pub fn to_db(&self) -> Value {
+        match *self {
+            Edge::Child(n) => hashmap!("id"    => Value::from(n),
+                                       "class" => Value::from("child"))
+                .into(),
+            Edge::Next(n) => hashmap!("id"    => Value::from(n),
+                                      "class" => Value::from("next"))
+                .into(),
+        }
+    }
+
+    pub fn from_db(val: Value) -> Result<Edge, &'static str> {
+        match val {
+            Value::Structure {
+                signature,
+                mut fields,
+            } => {
+                assert_eq!(signature, 0x52);
+                assert_eq!(fields.len(), 5);
+                let dest_id = NodeID::new(fields
+                    .remove(2)
+                    .as_int()
+                    .ok_or("DestID field is not an Integer")?);
+                let class = fields
+                    .remove(3)
+                    .as_map()
+                    .and_then(|mut i| i.remove("class"))
+                    .and_then(Value::as_string)
+                    .ok_or(
+                        "Edge class property missing, not a string or properties field not a map",
+                    )?;
+                match &class[..] {
+                    "child" => Ok(Edge::Child(dest_id)),
+                    "next" => Ok(Edge::Next(dest_id)),
+                    _ => Err("Invalid edge class"),
+                }
+            }
+            _ => Err("Value is not an edge"),
+        }
+    }
+}
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,14 +1,14 @@
 mod nodeid;
 mod gen_node;
 pub mod node_types;
+mod edge;
 
-use std::collections::HashMap;
 use packstream::values::Value;
 use uuid::Uuid5;
-use value_as::CastValue;
 
 pub use self::nodeid::NodeID;
 pub use self::gen_node::GenNode;
+pub use self::edge::Edge;
 
 use self::node_types::EnumNode;
 
@@ -38,56 +38,4 @@ pub trait Generable: HasID + HasUUID {
     fn new(id: NodeID, uuid: Uuid5, additional: Option<Self::Additional>) -> Self
     where
         Self: Sized;
-}
-
-pub enum Edge {
-    Child(NodeID),
-    Next(NodeID),
-}
-
-impl Edge {
-    pub fn to_db(&self) -> Value {
-        let mut prop: HashMap<&'static str, Value> = HashMap::new();
-        match *self {
-            Edge::Child(n) => {
-                prop.insert("id", n.into());
-                prop.insert("class", "child".into());
-            }
-            Edge::Next(n) => {
-                prop.insert("id", n.into());
-                prop.insert("class", "next".into());
-            }
-        }
-        prop.into()
-    }
-
-    pub fn from_db(val: Value) -> Result<Edge, &'static str> {
-        match val {
-            Value::Structure {
-                signature,
-                mut fields,
-            } => {
-                assert_eq!(signature, 0x52);
-                assert_eq!(fields.len(), 5);
-                let dest_id = NodeID::new(fields
-                    .remove(2)
-                    .as_int()
-                    .ok_or("DestID field is not an Integer")?);
-                let class = fields
-                    .remove(3)
-                    .as_map()
-                    .and_then(|mut i| i.remove("class"))
-                    .and_then(Value::as_string)
-                    .ok_or(
-                        "Edge class property missing, not a string or properties field not a map",
-                    )?;
-                match &class[..] {
-                    "child" => Ok(Edge::Child(dest_id)),
-                    "next" => Ok(Edge::Next(dest_id)),
-                    _ => Err("Invalid edge class"),
-                }
-            }
-            _ => Err("Value is not an edge"),
-        }
-    }
 }
